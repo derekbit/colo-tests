a86becbaf1c8038608e54edf5d131bb79b603ad2
Derek Su <dereksu@qnap.com>
2020-08-28 06:40:40 +0000

    Add batch-run.sh

---
+++ batch-run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+IODEPTH=64
+FILESIZE="72G"
+
+HOST_IP="10.20.91.101"
+STORAGE_IP="10.20.91.103"
+
+
+NAME=ft_with_write_throttle
+
+mkdir $NAME
+
+for BS in "4K" "16K" "64K"
+do
+	for DIRECT in 0 1
+	do
+		WORKING_DIR=`pwd`/${DIRECT}/${BS}
+
+		[ ! -e ${WORKING_DIR} ] || rm -rf ${WORKING_DIR}
+		mkdir -p ${WORKING_DIR}
+
+		bash run-tests.sh --iodepth ${IODEPTH} --size ${FILESIZE} --blocksize ${BS} --direct ${DIRECT} --host ${HOST_IP} --storage ${STORAGE_IP} ${WORKING_DIR} fio-tests/*.fio
+		[ ! -e ${WORKING_DIR}/testfile ] || rm -f ${WORKING_DIR}/testfile
+	done
+done
